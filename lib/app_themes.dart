9ceafc22d60027ef8b06c5b5bc52d99adda4bcb2
chris-am <c.aranguizm@outlook.com>
2022-04-02 20:02:55 -0300

    building personalized app theme

---
+++ lib/app_themes.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primary = Colors.orange;
+  static final ThemeData lightTheme = ThemeData.light().copyWith(
+    //primary color: indigo
+    primaryColor: primary,
+    //app bars
+    appBarTheme: const AppBarTheme(
+      color: primary,
+      elevation: 0,
+    ),
+    //text buttons
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(primary: primary),
+    ),
+    //floating buttons
+    floatingActionButtonTheme:
+        const FloatingActionButtonThemeData(backgroundColor: primary),
+    //elevated buttons
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        primary: primary,
+        shape: const StadiumBorder(),
+        elevation: 0,
+      ),
+    ),
+    //inputs
+    inputDecorationTheme: const InputDecorationTheme(
+      floatingLabelStyle: TextStyle(color: primary),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: primary),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(10),
+          bottomRight: Radius.circular(10),
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: primary),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(10),
+          bottomRight: Radius.circular(10),
+        ),
+      ),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(10),
+          bottomRight: Radius.circular(10),
+        ),
+      ),
+    ),
+  );
+  //dark
+  static final ThemeData darkTheme = ThemeData.dark().copyWith(
+    primaryColor: primary,
+    appBarTheme: const AppBarTheme(
+      color: primary,
+      elevation: 0,
+    ),
+    scaffoldBackgroundColor: Colors.black,
+  );
+}
