cd6658a416a51a6344874d244a51cd81f99ae18b
chris-am <c.aranguizm@outlook.com>
2022-04-02 20:16:53 -0300

    creating custom widgets

---
+++ lib/widgets/card_swiper.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CardSwiperScreen extends StatelessWidget {
+  const CardSwiperScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+    );
+  }
+}
