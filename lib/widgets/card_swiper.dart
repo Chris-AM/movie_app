2855c94ea5a7a1165fae2c56b5514ef8e5d51561
chris-am <c.aranguizm@outlook.com>
2022-04-10 00:20:58 -0400

    building swipper widget and injecting it in home screen

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -1,15 +1,27 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
-import 'package:movie_app/app_themes.dart';
 
 class CardSwiperScreen extends StatelessWidget {
   const CardSwiperScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    final size = MediaQuery.of(context).size;
+
+    return SizedBox(
       width: double.infinity,
-      height: 500,
-      color: AppTheme.secondary,
+      height: size.height * 0.5,
+      child: Swiper(
+        itemCount: 10,
+        layout: SwiperLayout.STACK,
+        itemWidth: size.width * 0.6,
+        itemHeight: size.height * 0.9,
+        itemBuilder: (_, int index) {
+          return const FadeInImage(
+              placeholder: AssetImage('assets/loading.gif'),
+              image: NetworkImage('https://via.placeholder.com/350x150'));
+        },
+      ),
     );
   }
 }
