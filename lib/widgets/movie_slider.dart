e5dae251154d2da0ddf64a5c589d676f8de1c172
chris-am <c.aranguizm@outlook.com>
2022-04-10 01:21:33 -0400

    building carrousel waiting to connect to api

--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -8,28 +8,31 @@ class MovieSlider extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Container(
-        width: double.infinity,
-        height: 250,
-        color: Colors.amber,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(left: 20, top: 20),
-              child: Text(
-                'Popular Movies',
-                style: AppTheme.title,
-              ),
+      width: double.infinity,
+      height: 260,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: 20, top: 20),
+            child: Text(
+              'Popular Movies',
+              style: AppTheme.title,
             ),
-            Expanded(
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 20,
-                itemBuilder: (_, int index) => const _MoviePoster(),
-              ),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Expanded(
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 20,
+              itemBuilder: (_, int index) => const _MoviePoster(),
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
 
@@ -40,9 +43,39 @@ class _MoviePoster extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 130,
-      height: 190,
-      color: Colors.green,
-      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      height: 260,
+      margin: const EdgeInsets.symmetric(
+        horizontal: 10,
+      ),
+      child: Column(
+        children: <Widget>[
+          GestureDetector(
+            onTap: () => Navigator.pushNamed(context, 'details',
+                arguments: 'movie-information'),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: const FadeInImage(
+                placeholder: AssetImage('assets/loading.gif'),
+                image: NetworkImage('https://via.placeholder.com/300x400'),
+                width: 130,
+                height: 190,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          const Flexible(
+            child: Text(
+              'Quis ad sunt incididunt aliqua veniam quis dolor cupidatat.',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.center,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
