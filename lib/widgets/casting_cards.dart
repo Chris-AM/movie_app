fa75580f6e734e384301fadef0ecf4ddbea78457
chris-am <c.aranguizm@outlook.com>
2022-04-10 21:50:07 -0400

    creating footer detail carrousel and filling it with actors dummy data

--- lib/widgets/casting_cards.dart
+++ lib/widgets/casting_cards.dart
@@ -7,9 +7,49 @@ class CastingCards extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: const EdgeInsets.only(bottom: 30),
       width: double.infinity,
       height: 180,
-      color: AppTheme.primary,
+      child: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (_, int index) => const _CastCards(),
+        scrollDirection: Axis.horizontal,
+      ),
+    );
+  }
+}
+
+class _CastCards extends StatelessWidget {
+  const _CastCards({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      width: 110,
+      height: 100,
+      child: Column(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/loading.gif'),
+              image: NetworkImage(
+                'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2020/10/kimetsu-no-yaiba-movie-mugen-ressha-hen-2106843.jpg?itok=RjM5j4Xi',
+              ),
+              fit: BoxFit.cover,
+              height: 140,
+              width: 100,
+            ),
+          ),
+          Text(
+            'actor.name',
+            maxLines: 2,
+            style: AppTheme.footer,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
     );
   }
 }
