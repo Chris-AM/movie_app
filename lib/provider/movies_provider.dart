934cf38e023eda4374a8d8b9733f79c50c7f553e
chris-am <c.aranguizm@outlook.com>
2022-04-11 00:15:28 -0400

    fetching popular movie data and mapping with popular model

--- lib/provider/movies_provider.dart
+++ lib/provider/movies_provider.dart
@@ -7,12 +7,15 @@ class MoviesProvider extends ChangeNotifier {
   final String _apiKey = 'd270788ca2d112fed4a52320da827816';
   final String _baseUrl = 'api.themoviedb.org';
   final String _nowPlayingPath = '3/movie/now_playing';
+  final String _popularPath = '3/movie/popular';
   final String _language = 'en-US';
 
   List<Movie> nowPlayingMovies = [];
+  List<Movie> popularMovies = [];
 
   MoviesProvider() {
     getNowPlayingMovieFromDB();
+    getPopularMoviesFromDB();
   }
   getNowPlayingMovieFromDB() async {
     var url = Uri.https(
@@ -27,5 +30,23 @@ class MoviesProvider extends ChangeNotifier {
     final response = await http.get(url);
     final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
     nowPlayingMovies = nowPlayingResponse.results;
+    notifyListeners();
+  }
+
+  getPopularMoviesFromDB() async {
+    var url = Uri.https(
+      _baseUrl,
+      _popularPath,
+      {
+        'api_key': _apiKey,
+        'language': _language,
+        'page': '1',
+      },
+    );
+    final response = await http.get(url);
+    final popularResponse = PopularResponse.fromJson(response.body);
+    popularMovies = [...popularMovies, ...popularResponse.results];
+    print('first popular movies ===> ${popularMovies[0]}');
+    notifyListeners();
   }
 }
