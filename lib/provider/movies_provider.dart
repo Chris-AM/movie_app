c9803679d1079f0ee5465d22af13c1ddfa267cd1
chris-am <c.aranguizm@outlook.com>
2022-04-10 22:36:26 -0400

    connecting app to the movie db

--- lib/provider/movies_provider.dart
+++ lib/provider/movies_provider.dart
@@ -1,11 +1,30 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier {
+  final String _apiKey = 'd270788ca2d112fed4a52320da827816';
+  final String _baseUrl = 'api.themoviedb.org';
+  final String _nowPlayingPath = '3/movie/now_playing';
+  final String _language = 'en-US';
+
   MoviesProvider() {
-    print('Movies provider initializated');
     getNowPlayingMovieFromDB();
   }
   getNowPlayingMovieFromDB() async {
-    print('Get now playing movies from db');
+    var url = Uri.https(
+      _baseUrl,
+      _nowPlayingPath,
+      {
+        'api_key': _apiKey,
+        'language': _language,
+        'page': '1',
+      },
+    );
+    final response = await http.get(url);
+    final Map<String, dynamic> decodedData = json.decode(response.body) ;
+    if (response.statusCode != 200) return print('error');
+    print(decodedData['dates']);
   }
 }
