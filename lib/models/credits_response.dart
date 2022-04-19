dee67003a82175d2b335d730d8278a1839218eed
chris-am <c.aranguizm@outlook.com>
2022-04-18 23:21:20 -0400

    model which maps the credits response added

---
+++ lib/models/credits_response.dart
@@ -0,0 +1,87 @@
+// To parse this JSON data, do
+//
+//     final creditsResponse = creditsResponseFromMap(jsonString);
+
+import 'dart:convert';
+
+class CreditsResponse {
+  CreditsResponse({
+    required this.id,
+    required this.cast,
+    required this.crew,
+  });
+
+  int id;
+  List<Cast> cast;
+  List<Cast> crew;
+
+  factory CreditsResponse.fromJson(String str) =>
+      CreditsResponse.fromMap(json.decode(str));
+
+  factory CreditsResponse.fromMap(Map<String, dynamic> json) => CreditsResponse(
+        id: json["id"],
+        cast: List<Cast>.from(json["cast"].map((x) => Cast.fromMap(x))),
+        crew: List<Cast>.from(json["crew"].map((x) => Cast.fromMap(x))),
+      );
+}
+
+class Cast {
+  Cast({
+    required this.adult,
+    required this.gender,
+    required this.id,
+    required this.knownForDepartment,
+    required this.name,
+    required this.originalName,
+    required this.popularity,
+    this.profilePath,
+    required this.castId,
+    this.character,
+    this.creditId,
+    this.order,
+    this.department,
+    this.job,
+  });
+
+  bool adult;
+  int gender;
+  int id;
+  String knownForDepartment;
+  String name;
+  String originalName;
+  double popularity;
+  String? profilePath;
+  int? castId;
+  String? character;
+  String? creditId;
+  int? order;
+  String? department;
+  String? job;
+
+  get fullProfileImg {
+    var _imgUrl = 'https://image.tmdb.org/t/p/w500$profilePath';
+    var noImg =
+        'http://www.newdesignfile.com/postpic/2015/02/funny-no-image-available-icon_68017.jpg';
+    if (profilePath != null) return _imgUrl;
+    return noImg;
+  }
+
+  factory Cast.fromJson(String str) => Cast.fromMap(json.decode(str));
+
+  factory Cast.fromMap(Map<String, dynamic> json) => Cast(
+        adult: json["adult"],
+        gender: json["gender"],
+        id: json["id"],
+        knownForDepartment: json["known_for_department"],
+        name: json["name"],
+        originalName: json["original_name"],
+        popularity: json["popularity"].toDouble(),
+        profilePath: json["profile_path"],
+        castId: json["cast_id"],
+        character: json["character"],
+        creditId: json["credit_id"],
+        order: json["order"],
+        department: json["department"],
+        job: json["job"],
+      );
+}
