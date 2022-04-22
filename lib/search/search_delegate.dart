274b0e68843d13ecc7e28683d233400653bced94
chris-am <c.aranguizm@outlook.com>
2022-04-22 16:18:39 -0400

    searching movies

--- lib/search/search_delegate.dart
+++ lib/search/search_delegate.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/app_themes.dart';
+import 'package:movie_app/models/models.dart';
+import 'package:movie_app/provider/movies_provider.dart';
+import 'package:provider/provider.dart';
 
 class MovieSearchDelegate extends SearchDelegate {
   @override
@@ -25,21 +28,66 @@ class MovieSearchDelegate extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    return const Text('build results');
+    if (query.isEmpty) {
+      return _emptyContainer();
+    }
+
+    final moviesProvider = Provider.of<MoviesProvider>(context, listen: false);
+
+    return FutureBuilder(
+      future: moviesProvider.searchMovies(query),
+      builder: (_, AsyncSnapshot<List<Movie>> snapshot) {
+        if (!snapshot.hasData) return _emptyContainer();
+        final movies = snapshot.data;
+        return ListView.builder(
+          itemCount: movies!.length,
+          itemBuilder: (_, int index) => _MovieItem(
+            movie: movies[index],
+          ),
+        );
+      },
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    if (query.isEmpty) {
-      return const SizedBox(
-          child: Center(
+    return _emptyContainer();
+  }
+
+  Widget _emptyContainer() {
+    return const SizedBox(
+      child: Center(
         child: Icon(
           Icons.movie_creation_outlined,
           color: AppTheme.primary,
           size: 100,
         ),
-      ));
-    }
-    return SizedBox();
+      ),
+    );
+  }
+}
+
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+  const _MovieItem({
+    Key? key,
+    required this.movie,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: FadeInImage(
+        placeholder: const AssetImage('assets/no-image.jpg'),
+        image: NetworkImage(movie.fullPosterImg),
+        width: 50,
+        fit: BoxFit.contain,
+      ),
+      title: Text(movie.title),
+      subtitle: Text(movie.originalTitle),
+      onTap: () {
+        Navigator.pushNamed(context, 'details', arguments: movie);
+      },
+    );
   }
 }
